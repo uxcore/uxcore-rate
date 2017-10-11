e74d97cb0a69d90800d30e3855be76fe61d15df4
xiaohe.wp <xiaohe.wp@alibaba-inc.com>
2017-10-11 19:46:34 +0800

    统一this的使用方式

--- src/Rate.jsx
+++ src/Rate.jsx
@@ -93,7 +93,7 @@ class Rate extends React.Component {
     return (
       <div className={`${t.props.prefixCls}-always-tip-container`} ref={(c) => { this.alwaysTip = c; }}>
         {
-          t.state.hover === 0 ? this.props.placeholder : t.props.scoreTips[parseInt(t.state.hover, 10) - 1]
+          t.state.hover === 0 ? t.props.placeholder : t.props.scoreTips[parseInt(t.state.hover, 10) - 1]
         }
       </div>
     );
